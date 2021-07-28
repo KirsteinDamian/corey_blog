ee66c87b59cbca5ceac759fc2e30b68f30aa899a
Damian Kirstein <kirsteindamian@gmail.com>
2021-07-28 21:06:51 +0200

    Blog and about templates (part 3)

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Damian',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date': '28.07.2021',
+    },
+    {
+        'author': 'Pawel',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date': '29.07.2021',
+    },
+]
 
 
 def home(request):
-    return HttpResponse('Hello Word!')
+    context = {
+        'posts': posts,
+        'title': 'Dummy title'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
