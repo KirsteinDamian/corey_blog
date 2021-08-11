847e68c70a1154e9c7ce0c5349a1464dcdb45823
Damian Kirstein <kirsteindamian@gmail.com>
2021-08-11 22:28:57 +0200

    User registration (Part 6)

--- corey_blog/urls.py
+++ corey_blog/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from blog.views import home
+from users.views import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('register/', register, name='register'),
 ]
