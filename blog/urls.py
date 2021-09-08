059a9490b1df384bd543dca0a179e90dd83286c1
Damian Kirstein <kirsteindamian@gmail.com>
2021-09-08 23:41:47 +0200

    User login system (Part 7)

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from blog.views import home, about
+from users.views import register, profile
 
 urlpatterns = [
     path('', home, name='blog-home'),
     path('about/', about, name='blog-about'),
+    path('login/',
+         auth_views.LoginView.as_view(template_name='users/login.html'),
+         name='login',
+         ),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='users/logout.html'),
+         name='logout',
+         ),
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile'),
 ]
--- corey_blog/urls.py
+++ corey_blog/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from users.views import register
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('register/', register, name='register'),
 ]
