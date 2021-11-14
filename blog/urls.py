4644c90f6ef6969b219457a167d014810857b77a
Damian Kirstein <kirsteindamian@gmail.com>
2021-11-14 17:11:10 +0100

    Pagination (Part 11)

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from blog.views import about, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from blog.views import (about, PostListView, PostDetailView, PostCreateView,
+                        PostUpdateView, PostDeleteView, UserPostListView)
 from users.views import register, profile
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
